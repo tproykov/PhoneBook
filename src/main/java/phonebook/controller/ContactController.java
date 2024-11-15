cc83afe30af7270b6915b69f72c08a0587b824f3
tproykov <tproykov@gmail.com>
2024-11-15 00:48:22 +0000

    Update ContactController.java

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
@@ -29,15 +30,27 @@ public class ContactController {
 
     @GetMapping("/edit{name}")
     private ModelAndView editView(@PathVariable String name, ModelAndView modelAndView) {
-        Contact contactToEdit = this.contacts.stream().filter(c -> c.getName().equals(name)).collect(Collectors.toList()).get(0);
+        Contact contactToEdit = getContactToEdit(name);
         modelAndView.setViewName("edit");
         modelAndView.addObject(contactToEdit);
         return modelAndView;
+    }
+
+    @PutMapping("/edit{name}")
+    private String edit(@PathVariable String name, Contact contact) {
+        Contact editedContact = getContactToEdit(name);
+        editedContact.setName(contact.getName());
+        editedContact.setNumber(contact.getNumber());
+        return "redirect:/";
     }
 
     @PostMapping("/")
     private String addContact(Contact contact) {
         this.contacts.add(contact);
         return "redirect:/";
+    }
+
+    private Contact getContactToEdit(String name) {
+        return this.contacts.stream().filter(c -> c.getName().equals(name)).collect(Collectors.toList()).get(0);
     }
 }
